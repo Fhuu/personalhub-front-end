3c061b7eddb7a17948c7f14609db214f37cbf0d2
s0566367 <diro.baloska@student.htw-berlin.de>
2020-08-09 16:14:57 +0200

    Created container for side menu

---
+++ personalhub/src/js/app/SideMenu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../css/SideMenu.scss'
+
+export default class SideMenu extends React.Component {
+    render() {
+        return (
+            <div id="side-menu">
+                <div>This is the side menu</div>
+            </div>
+        )
+    }
+}
