8ee9f04ebae2eb319caaff54ebfba572bd283c05
s0566367 <diro.baloska@student.htw-berlin.de>
2020-08-09 17:32:14 +0200

    create Footer React Component

---
+++ personalhub/src/js/app/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../css/Footer.scss';
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <div id="footer">
+                <span>Created by React - Self Project</span>
+                <span>Contact me.</span>
+            </div>
+        )
+    }
+}
