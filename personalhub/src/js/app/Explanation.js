788c7780483dda9148ae739f20155a2e94287ad7
s0566367 <diro.baloska@student.htw-berlin.de>
2020-08-09 16:14:01 +0200

    some expalanation of the website

---
+++ personalhub/src/js/app/Explanation.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../../css/Explanation.scss';
+
+class Explanation extends React.Component {
+    render () {
+        return (
+            <div id="explanation">
+                <div className="explanations" id="explanation1">
+                    <div className="content-container">
+                        <p>
+                            Lets Meet adalah Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                            At vero eos et accusam et justo duo dolores et ea rebum. 
+                            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        </p>
+                    </div>
+                    
+                </div>
+
+                <div className="explanations" id="explanation2">
+                    <div className="content-container">
+                        <p>
+                            The platform is to lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                            At vero eos et accusam et justo duo dolores et ea rebum. 
+                            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="explanations" id="explanation3">
+                    <div className="content-container">
+                        <p>
+                            Me, the creator is to lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                            At vero eos et accusam et justo duo dolores et ea rebum. 
+                            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        </p>
+                    </div>
+                </div>
+
+
+            </div>
+        )
+    }
+}
+
+export default Explanation;
