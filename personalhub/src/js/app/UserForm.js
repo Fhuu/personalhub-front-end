11a657df59299ac0d25ea3d1ff5f4ba34afe8d1a
s0566367 <diro.baloska@student.htw-berlin.de>
2020-08-09 16:56:25 +0200

    sign in/up form

---
+++ personalhub/src/js/app/UserForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class UserForm extends React.Component {
+    render() {
+        return (
+            <div id="user-form">
+                This is where user form should be shown
+            </div>
+        )
+    }
+}
