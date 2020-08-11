9afbe91730ee4f9a066d199cdcf7b10e63ecb53d
s0566367 <diro.baloska@student.htw-berlin.de>
2020-08-11 21:12:46 +0200

    show-login-button and render login form to portal

--- personalhub/src/js/app/UserForm/UserForm.js
+++ personalhub/src/js/app/UserForm/UserForm.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import Form from './Form'
-
+import LoginForm from './LoginForm'
 export default class UserForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showForm : false
+        }
+    }
+
+    changeStateToShowForm = () => {
+        this.setState({showForm : true})
+    }
+
+    showForm = () => {
+        if(this.state.showForm) {
+            return (<LoginForm />)
+        }
+        return(
+            <li id="show-login-form" onClick={this.changeStateToShowForm}>Log In</li>
+        )
+        
+    }
+
     render() {
         return (
             <div id="user-form">
-                <Form />
+                <this.showForm />
             </div>
         )
     }
