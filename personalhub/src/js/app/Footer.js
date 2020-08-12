a3a357a3a01adf0883c1b3ecd6e0b47cf9e72503
s0566367 <diro.baloska@student.htw-berlin.de>
2020-08-12 02:50:35 +0200

    Logo source and style

--- personalhub/src/js/app/Footer.js
+++ personalhub/src/js/app/Footer.js
@@ -7,6 +7,7 @@ export default class Footer extends React.Component {
             <div id="footer">
                 <span>Created by React <b>-</b> Self Project</span>
                 <span>Contact me.</span>
+                <span>Logo created by <b><a href="https://studio.tailorbrands.com/">https://studio.tailorbrands.com/</a></b></span>
             </div>
         )
     }
